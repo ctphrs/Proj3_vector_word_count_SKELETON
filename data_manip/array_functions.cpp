62a7292ca04cd1b29fe4729765d922e643a1a522
danny <danny@danny-VirtualBox>
2020-10-09 00:20:26 -0400

    processToken working?

--- data_manip/array_functions.cpp
+++ data_manip/array_functions.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include "../includes/constants.h"
 #include "../includes/array_functions.h"
+#include "../includes/utilities.h"
 
 namespace KP{
 	//zero out vector that tracks words and their occurrences
@@ -26,29 +27,67 @@ namespace KP{
 		return entries[i].number_occurences;
 	}
 
-
-	/*Keep track of how many times each token seen*/
-		void processToken(std::vector<constants::entry>  &entries,std::string &token){
-
-		}
-	/*take 1 line and extract all the tokens from it
-	feed each token to processToken for recording*/
-	void processLine(std::vector<constants::entry>  &entries,std::string &myString){
-
-	}
 	/*loop through whole file, one line at a time
 		 * call processLine on each line from the file
 		 * returns false: myfstream is not open
 		 *         true: otherwise*/
 	bool processFile(std::vector<constants::entry>  &entries,std::fstream &myfstream){
 			std::string str;
+			if(!myfstream.is_open())
+				return false;
 			while(getline(myfstream, str)){
 					processLine(entries, str);
 			}
-			return myfstream.is_open();
+			return true;
 		}
+	/*take 1 line and extract all the tokens from it
+ 	 feed each token to processToken for recording*/
+	void processLine(std::vector<constants::entry> &entries, std::string &myString) {
+		std::stringstream ss(myString);
+		std::string tempToken;
 
+		while(getline(ss, tempToken, constants::CHAR_TO_SEARCH_FOR)){
+			processToken(entries, tempToken);
+		}
+	}
+	/*Keep track of how many times each token seen*/
+	void processToken(std::vector<constants::entry> &entries, std::string &token) {
 
+		if(!strip_unwanted_chars(token)){
+		            return;
+		        }
+		        int index = -1;
+		        for(int i = 0; i < entries.size(); i++){
+		            if (entries[i].word == token){
+		                index = i;
+		                break;
+		            }
+		        }
+		        if (index == -1){
+		            std::string upper_token = token;
+		            toUpper(upper_token);
+		            constants::entry new_entry = {token, upper_token, 1};
+		            entries.push_back(new_entry);
+		        }
+		        else {
+		            entries[index].number_occurences += 1;
+		        }
+//Coulf not get this code to work so did it another way
+//		for(int i=0; i<entries.size(); i++){
+//			strip_unwanted_chars(token);
+//		           if(token!=entries[i].word){
+//		               // Make a version of the token thats uppercase to store in our new struct
+//		               std::string token_upper = token;
+//		               toUpper(token_upper);
+//		               // Make a new entry struct with the value of token
+//		               constants::entry entry_token{token, token_upper,1};
+//		               entries.push_back(entry_token);
+//		                }
+//		                if(token==entries[i].word){
+//		                    entries[i].number_occurences++;
+//		            }
+//		        }
+	}
 	/*
 	 * Sort myEntryArray based on so enum value.
 	 * Please provide a solution that sorts according to the enum
